4dd7f32df40b8ae8cecc6e7e5150154abc248d07
podestal <luisrodriguezpodesta@gmail.com>
2023-10-25 13:37:05 -0400

    Rock Paper Scissors Game

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # python-beginer-projects
+This repostitory is going to hold a list of beginer python prjects, each project is going to be specific for a concept in python:
+1. Rock, paper scissors -> List, Random
---
+++ rock-paper-scissors.py
@@ -0,0 +1,44 @@
+################### Rock Paper Scissors Game ###################
+
+import random
+
+movesList = ['Rock', 'Paper', 'Scissors']
+# machineGuess = random.randint(0,2)
+
+print('Welcome to the game pick the following numbers.')
+print('1 -> Rock')
+print('2 -> Paper')
+print('3 -> Scissors')
+
+machine = movesList[0]
+userGuess = movesList[int(input('Which one do you choose: ')) - 1]
+
+print('You choose: ', userGuess)
+print('Machine is: ', machine)
+
+if machine == 'Rock':
+    match userGuess:
+        case 'Rock':
+            print('even')
+        case 'Scissors':
+            print('You lost')
+        case 'Paper':
+            print('You won')
+elif machine == 'Paper':
+    match userGuess:
+        case 'Paper':
+            print('even')
+        case 'Rock':
+            print('You lost')
+        case 'Scissors':
+            print('You won')
+elif machine == 'Scissors':
+    match userGuess:
+        case 'Scissors':
+            print('even')
+        case 'Paper':
+            print('You lost')
+        case 'Rock':
+            print('You won')
+
+
